1139ce48fba7ba77ed9bb856379991c6a8dec102
franklynch <124372021+franklynch@users.noreply.github.com>
2025-02-14 07:55:30 +0000

    End of E11

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -4,11 +4,13 @@
 #include "Hazel/Events/ApplicationEvent.h"
 #include "Hazel/Log.h"
 
+#include <GLFW/glfw3.h>
+
 namespace Hazel {
 
 	Application::Application()
 	{
-
+		m_Window = std::unique_ptr<Window>(Window::Create());
 	}
 
 	Application::~Application()
@@ -18,10 +20,14 @@ namespace Hazel {
 
 	void Application::Run()
 	{
-		WindowResizeEvent e(1200, 720);
-		HZ_TRACE(e.ToString());
+	
 
-		while(true);
+		while (m_Running)
+		{
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
+			m_Window->OnUpdate();
+		}
 	}
 
 }
--- premake5.lua
+++ premake5.lua
@@ -11,6 +11,12 @@ workspace "Hazel"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}" 
 
+IncludeDir = {}
+IncludeDir["GLFW"] = "Hazel/vendor/GLFW/include"
+
+include "Hazel/vendor/GLFW"
+
+
 
 
 project "Hazel"
@@ -36,9 +42,15 @@ project "Hazel"
 		includedirs
 		{
 			"%{prj.name}/src/",
-			"%{prj.name}/vendor/spdlog/include"
+			"%{prj.name}/vendor/spdlog/include",
+			"%{IncludeDir.GLFW}"
 		}
 
+		links
+		{
+			"GLFW",
+			"opengl32.lib"
+		}
 		
 
 	
