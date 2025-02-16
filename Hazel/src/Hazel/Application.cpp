7b421a4e64ff185330caae6c444de04843357c2e
franklynch <124372021+franklynch@users.noreply.github.com>
2025-02-16 14:40:45 +0000

    ImGuI step 1

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -11,8 +11,13 @@ namespace Hazel {
 
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
 
+	Application* Application::s_Instance = nullptr;
+
+
 	Application::Application()
 	{
+		HZ_CORE_ASSERT(!s_Instance, "Application Already Exists !")
+		s_Instance = this;
 		m_Window = std::unique_ptr<Window>(Window::Create());
 		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
@@ -25,11 +30,13 @@ namespace Hazel {
 	void Application::PushLayer(Layer* layer)
 	{
 		m_LayerStack.PushLayer(layer);
+		layer->OnAttach();
 	}
 
 	void Application::PushOverlay(Layer* layer)
 	{
 		m_LayerStack.PushOverlay(layer);
+		layer->OnAttach();
 	}
 
 	void Application::OnEvent(Event& e)
@@ -37,7 +44,7 @@ namespace Hazel {
 		EventDispatcher dispatcher(e);
 		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
 		
-		HZ_CORE_TRACE("{0}", e.ToString());
+		// HZ_CORE_TRACE("{0}", e.ToString());
 
 		for (auto it = m_LayerStack.end(); it != m_LayerStack.begin(); )
 		{
--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -24,6 +24,9 @@ namespace Hazel {
 		void PushLayer(Layer* layer);
 		void PushOverlay(Layer* layer);
 
+		inline static Application& Get() { return *s_Instance;  }
+		inline Window& GetWindow() { return *m_Window; }
+
 
 	private:
 		bool OnWindowClose(WindowCloseEvent& e);
@@ -31,6 +34,9 @@ namespace Hazel {
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 		LayerStack m_LayerStack;
+
+	private:
+		static Application* s_Instance;
 	};
 
 	//TO BE DEFINED IN CLIENT
